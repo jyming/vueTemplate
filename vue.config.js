b194b61e0a495f3331c477af84b29a4abcfe817c
jym <1253264340@qq.com>
2020-06-23 16:01:19 +0800

    初始提交

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "智能办公室"
+    }
+  },
+  publicPath: "./",
+  outputDir: "p_h5",
+  assetsDir: "static",
+  productionSourceMap: false
+};
